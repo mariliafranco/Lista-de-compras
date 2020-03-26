e7a2537f7148b879ec99d7fc399e85580f6ae542
mariliafranco <marilia.andrad@gmail.com>
2020-03-26 16:58:04 -0300

    first commit

---
+++ src/ListaDeCompras.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Item from './Item';
+
+function ListaDeCompras() {
+  return (
+    <>
+      <Item nome="leite" />
+      <Item nome="ovos" />
+      <Item nome="chocolate" />
+      <Item nome="pão" />
+      <Item nome= "nescau" />
+    </>
+  );
+}
+
+export default ListaDeCompras;
